7141fbcf95eed9b110a36a8b7d6ca54c0d7b62ee
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-05-16 15:21:04 +0300

    Add more static constructors to Delay

--- src/Async/Delay.php
+++ src/Async/Delay.php
@@ -12,7 +12,37 @@ use Telephantast\MessageBus\Stamp;
  */
 final readonly class Delay implements Stamp
 {
+    private const SECONDS_MULTIPLIER = 1000;
+    private const MINUTES_MULTIPLIER = self::SECONDS_MULTIPLIER * 60;
+    private const HOURS_MULTIPLIER = self::MINUTES_MULTIPLIER * 60;
+    private const DAYS_MULTIPLIER = self::HOURS_MULTIPLIER * 24;
+
     public function __construct(
-        public int $seconds,
+        public int $milliseconds,
     ) {}
+
+    public static function till(\DateTimeImmutable $time, \DateTimeImmutable $now = new \DateTimeImmutable()): self
+    {
+        return new self((int) $time->format('Uv') - (int) $now->format('Uv'));
+    }
+
+    public static function fromSeconds(int|float $seconds): self
+    {
+        return new self((int) round($seconds * self::SECONDS_MULTIPLIER));
+    }
+
+    public static function fromMinutes(int|float $minutes): self
+    {
+        return new self((int) round($minutes * self::MINUTES_MULTIPLIER));
+    }
+
+    public static function fromHours(int|float $hours): self
+    {
+        return new self((int) round($hours * self::HOURS_MULTIPLIER));
+    }
+
+    public static function fromDays(int|float $days): self
+    {
+        return new self((int) round($days * self::DAYS_MULTIPLIER));
+    }
 }
