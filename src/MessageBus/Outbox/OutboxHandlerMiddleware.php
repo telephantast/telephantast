afc078b02a6dd748c9a35fc4645c7e50711c3673
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2024-10-24 19:19:03 +0300

    Log publish exception in OutboxHandlerMiddleware

--- src/MessageBus/Outbox/OutboxHandlerMiddleware.php
+++ src/MessageBus/Outbox/OutboxHandlerMiddleware.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Telephantast\MessageBus\Outbox;
 
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Telephantast\MessageBus\Async\TransportPublish;
 use Telephantast\MessageBus\Handler\Pipeline;
 use Telephantast\MessageBus\MessageContext;
@@ -19,6 +21,7 @@ final class OutboxHandlerMiddleware implements Middleware
         private readonly OutboxStorage $outboxStorage,
         private readonly TransactionProvider $transactionProvider,
         private readonly TransportPublish $transportPublish,
+        private readonly LoggerInterface $logger = new NullLogger(),
     ) {}
 
     public function handle(MessageContext $messageContext, Pipeline $pipeline): mixed
@@ -42,7 +45,17 @@ final class OutboxHandlerMiddleware implements Middleware
         });
 
         if ($outbox->envelopes !== []) {
-            $this->transportPublish->publish($outbox->envelopes);
+            try {
+                $this->transportPublish->publish($outbox->envelopes);
+            } catch (\Throwable $exception) {
+                $this->logger->error('Failed to publish outboxed messages.', [
+                    'exception' => $exception,
+                    'message_class' => $messageContext->getMessageClass(),
+                    'handler_id' => $pipeline->id(),
+                    'envelope' => $messageContext->envelope,
+                ]);
+            }
+
             $this->outboxStorage->empty(null, $messageId);
         }
 
